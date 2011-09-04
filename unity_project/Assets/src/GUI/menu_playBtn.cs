634fd1665a631aeb489e2dcf1e6b0ea8223b2ae0
peach <paulinealtmann@peach.(none)>
2011-09-04 18:55:24 +0200

    GUIElement is working

--- unity_project/Assets/src/GUI/menu_playBtn.cs
+++ unity_project/Assets/src/GUI/menu_playBtn.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class menu_playBtn : GUIElement {
 	
-	public GUIStyle play_btn_style;
+	/*public GUIStyle play_btn_style;
 	
 	void OnGUI(){
 		if(GeneralScreenGUI.Button(mGUIManager, new Rect (595,132,256,256), "", play_btn_style)){
 			//mGUIManager.pushGUIState(new StartGUS(mGUIManager, mCamera));
 		}
-	}
+	}*/
 }
