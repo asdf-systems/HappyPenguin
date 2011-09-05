be6aba204050146136d354e524efb599485be984
Friedrich Wessel @Blackdragon <wessel@sadf-systems.de>
2011-09-06 01:48:45 +0200

    start wórking on water animation

---
+++ unity_project/Assets/Editor/PopulateMultiTexture.js
@@ -0,0 +1,57 @@
+// Add texture to array
+static var assetPath ="Assets/assets/water/textures/pPlane_";
+static var imageType ="jpg";
+
+
+
+@MenuItem ("Custom/Load MultiTextureObject")
+static function loadmultitexture () {
+	
+	
+	var objects=Selection.gameObjects; //.Find("MultiGUITexture");
+ 	if(objects.Length == 0){
+ 		print("Please Select Object");
+ 		return;
+ 	}
+ 	var s = objects[0].GetComponent("MovieTexture");
+ 	
+ 	if(s) {
+ 	
+		Debug.Log("Find Texture Object");
+		//not sure what the types are, so force them
+		var a=1;
+		var pngAsset : Texture;
+
+		var assets = new Array();
+		while (true){
+			//set the path of the asset
+        	var fillZero = "";
+        	/*if(a < 10 )
+        		fillZero = "000";
+        	else if(a < 100 )
+        		fillZero = "00";
+        	else if(a < 1000 )
+        		fillZero = "0";		*/
+        	
+    		var path = assetPath + fillZero + (a)+"." + imageType;
+    		Debug.Log("Asset Path looked for: " + path);
+    		//find the asset and assign it to pngAsset
+         	pngAsset=AssetDatabase.LoadAssetAtPath(path,Texture);
+         	
+         	if(!pngAsset) {
+         		Debug.Log("Texture not Found");
+         		break;
+         	}
+         	
+         	a++;
+         	assets.push(pngAsset);
+		}
+		s.textures = assets.ToBuiltin(Texture);
+	}
+	else {
+		print("Could not find a MovieTexture on Selected Object");
+	}
+    
+}
+function Update () {
+}
---
+++ unity_project/Assets/src/MovieTexture.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class MovieTexture : MonoBehaviour {
+
+	public int movieSpeedFPS; 
+	public Texture[] textures;
+	private float frameTime;
+	
+	private int currentFrameNumber;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Awake(){
+		frameTime = 0;
+		currentFrameNumber = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate(){
+		//if(!GameConfig.videoPlay)
+		//	return;
+		
+		frameTime += Time.deltaTime;
+		if(frameTime > 1/movieSpeedFPS){ // change frame
+			
+			currentFrameNumber ++;
+			frameTime = 0;
+			if(currentFrameNumber >= textures.Length)
+				currentFrameNumber = 0;
+				gameObject.renderer.material.SetTexture("_MainTex", textures[currentFrameNumber]);		
+		}
+	}
+	
+	
+ 
+ 
+
+}
