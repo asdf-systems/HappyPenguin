3e9488387b2263c07034d2e5dc5537e1b0ba5c3d
Alexander Surma <alexander.surma@gmail.com>
2011-10-21 17:09:24 +0200

    Posting on Facebook is possible

--- unity_project/Assets/Scripts/GUI/Elements/Wardrobe/PostOnFB.cs
+++ unity_project/Assets/Scripts/GUI/Elements/Wardrobe/PostOnFB.cs
@@ -2,13 +2,47 @@ using UnityEngine;
 using System.Collections;
 
 public class PostOnFB : UIElementBehaviour<GUIStatics> {
+	private const string MESSAGE = "I dressed up my Pux!";
 
 	protected override void showElements(){
 		GeneralScreenGUI.Box(guiStatics, new Rect(positionX,positionY,128,128), "Post on Facebook", currentStyle);
-		
+
 	}
-	
+
 	protected override void hit(){
-		// TODO
+		int perms = Facebook.compilePermissions(Facebook.Permission.PUBLISH_STREAM);
+		var fb = Facebook.getInstance();
+		fb.authorize(perms, success => {
+			if(success == Facebook.LOGGED_OUT) {
+				Debug.Log("Login failed");
+				return;
+			}
+			var imglink = generateImageLink();
+			fb.postToStream(
+				MESSAGE, // Message
+				imglink, // Image URL
+				"", // Link?
+				"Pux the Glaciator", // Title
+				"Fighting for his life", // Subtitle
+				"Get the game for iPhone and iPad on the AppStore.", // Description
+				(success2, data) => {
+					if(success2 == Facebook.REQUEST_FAIL) {
+						Debug.Log("Post failed");
+						return;
+					}
+			});
+		});
+	}
+
+	private string generateImageLink() {
+		string ret = "http://glaciator.asdf-systems.de/penguins/";
+
+		ret += GameStatics.getPlayerHat();
+		ret += ".";
+		ret += "none"; //GameStatics.getPlayerBelt()
+		ret += ".";
+		ret += "none"; //GameStatics.getPlayerShoes()
+		ret += ".png";
+		return ret;
 	}
 }
--- unity_project/Assets/Scripts/GameStatics.cs
+++ unity_project/Assets/Scripts/GameStatics.cs
@@ -4,22 +4,26 @@ using System.Collections;
 public static class GameStatics {
 
 	private static string penguinHat;
-	
+
 	static GameStatics(){
 		penguinHat = "Red_Hat";
 	}
 	public static float Points{
-		get; 
+		get;
 		set;
 	}
-	
+
 	public static void savePlayerHat(string name){
 		penguinHat = name;
-	}	
-	
+	}
+
+	public static string getPlayerHat() {
+		return penguinHat;
+	}
+
 	public static GameObject loadPlayerHat(){
 		Object resource = Resources.Load("Pux_Cloth/" + penguinHat);
 		GameObject go = GameObject.Instantiate(resource) as GameObject;
-		return go;	
+		return go;
 	}
 }
