612a37de45b19c2e5c22b79cad40f50c4af7a671
Alexander Wieser-Kuciel <krasshirsch@krasshirsch-mac.(none)>
2011-10-29 17:28:27 +0200

    balloons are here #2

---
+++ unity_project/Assets/Scripts/Entities/BalloonBehaviour.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+public sealed class BalloonBehaviour : EnvironmentEntityBehaviour
+{
+	public BalloonBehaviour() {
+		// nada yet
+	}
+	
+	public GameObject Balloon;
+	public GameObject Fish;
+}
+
+
---
+++ unity_project/Assets/Scripts/LifeSpawnBeacon.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Pux
+{
+	public sealed class LifeSpawnBeacon
+	{
+		private readonly GameObject _object;
+		
+		public LifeSpawnBeacon(GameObject @object) {
+			_object = @object;
+		}
+		
+		public bool IsOccupied {
+			get { return !(_object.transform.GetChildCount() == 0); }
+		}
+		
+		public GameObject UnityGameObject {
+			get { return _object; }
+		}
+		
+	}
+}
+
