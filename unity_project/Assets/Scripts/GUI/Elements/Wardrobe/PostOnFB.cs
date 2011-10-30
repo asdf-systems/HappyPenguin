89908d16530f1001e17b0f419e4a7e2f7e51604e
Friedrich Wessel @Blackdragon <wessel@sadf-systems.de>
2011-10-30 18:17:50 +0100

    add alert for fb in wardrobe

--- unity_project/Assets/Scripts/GUI/Elements/Wardrobe/PostOnFB.cs
+++ unity_project/Assets/Scripts/GUI/Elements/Wardrobe/PostOnFB.cs
@@ -4,8 +4,10 @@ using System;
 
 public class PostOnFB : UIElementBehaviour<GUIStatics> {
 	private const string MESSAGE = "I dressed up my Pux!";
-
+	public AlertTextBehaviour alert; 
+	
 	protected override void showElements(){
+		//alert.ShowText("Highscore Postets - test");
 		GeneralScreenGUI.Box(guiStatics, new Rect(positionX,positionY,512,512), "", currentStyle);
 	}
 
@@ -15,6 +17,7 @@ public class PostOnFB : UIElementBehaviour<GUIStatics> {
 		fb.authorize(perms, success => {
 			if(success == Facebook.LOGGED_OUT) {
 				Debug.Log("Login failed");
+				alert.ShowText("Login failed");
 				return;
 			}
 			var imglink = generateImageLink();
@@ -27,11 +30,14 @@ public class PostOnFB : UIElementBehaviour<GUIStatics> {
 				"Get the game for iPhone and iPad on the AppStore.", // Description
 				(success2, data) => {
 					if(success2 == Facebook.REQUEST_FAIL) {
+						alert.ShowText("Post failed");
 						Debug.Log("Post failed: "+data);
 						return;
 					}
 			});
+			alert.ShowText("Highscore Postet");	
 		});
+		
 	}
 
 	private string generateImageLink() {
