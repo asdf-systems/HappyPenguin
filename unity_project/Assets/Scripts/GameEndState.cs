1c619aeaefb53a32f91073605683fa65ef9b0296
Friedrich Wessel @Blackdragon <wessel@sadf-systems.de>
2011-10-30 12:19:46 +0100

    GameEnd with Highscore and PersonalHighscore and Name Input is finished

--- unity_project/Assets/Scripts/GameEndState.cs
+++ unity_project/Assets/Scripts/GameEndState.cs
@@ -13,26 +13,24 @@ public class GameEndState : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Data Path: " + Application.persistentDataPath);
+		//Debug.Log("Data Path: " + Application.persistentDataPath);
 		time = 0.0f;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if(firstCheck){
-			GameStatics.Points = 140;
-			int points = Convert.ToInt32(GameStatics.Points);
-			Debug.Log("Points: " + points);
-			string uname = getUsername();
-			if(uname != string.Empty){
-				addEntry(uname, points);
-			}
+			
+			//Debug.LogWarning("Points fix for testing!!!");
+			//	GameStatics.Points = 50;
+			
+			checkHighscore();
 			firstCheck = false;
 			
 		}
 		if(!timeFreeze)
 			time+= Time.deltaTime;
-		//Debug.Log("Update GameEndState, Time: " + time);
+		
 		if(time > 10){
 			Application.LoadLevel(0);
 		}
@@ -50,7 +48,7 @@ public class GameEndState : MonoBehaviour {
 			nameAlert.showText = true;
 			okayButton.showButton = true;
 			timeFreeze = true;
-		}
+		} 
 		return username;
 		
 	}
@@ -60,7 +58,14 @@ public class GameEndState : MonoBehaviour {
 			HighscoreServer.GetHighscore(data=>{
 				checkForNewHighscore(data);
 			})  );
+		
+		string uname = getUsername();
+		int points = Convert.ToInt32(GameStatics.Points);
+		if(uname != string.Empty){
+			addEntry(uname, points);
+		}
 	}
+	
 	
 	private void checkForNewHighscore(Entry[] data){
 		
@@ -71,8 +76,49 @@ public class GameEndState : MonoBehaviour {
 			position++;
 		}
 		if(position < 4){
-			
 			alertElement.ShowText("New Highscore!!\n Position: " + position); //, 8, new Vector2(alertElement.positionX, alertElement.positionY));
+			writeNewPersonalHighscore(FormatPoints(GameStatics.Points));
+		} else{ 
+			checkPersonalHighscore();
+		}
+		
+		
+		
+	}
+	
+	private void checkPersonalHighscore(){
+		string spoints = "0000000";
+		try{
+			spoints = LocalStorage.ReadUTF8File("personal_highscore");
+			if( Convert.ToInt32(spoints) < GameStatics.Points ) {
+				alertNewPersonalHighscore(FormatPoints(GameStatics.Points));
+			}
+			
+		}catch(Exception e){
+			Debug.LogWarning(e.Message);
+			alertNewPersonalHighscore(FormatPoints(GameStatics.Points));
+			
+		}
+	}
+	
+	private string FormatPoints(float points){
+		int iPoints = (int)points;
+		string sPoints = iPoints.ToString();
+		while(sPoints.Length < 7)
+			sPoints = "0" + sPoints;
+		return sPoints;
+		
+	}
+	private void alertNewPersonalHighscore(string spoints){
+		alertElement.ShowText("New Personal Highscore!\n"+GameStatics.Points);
+		writeNewPersonalHighscore(spoints);
+	}
+	
+	private void writeNewPersonalHighscore(string spoints){
+		try{
+			LocalStorage.WriteUTF8File("personal_highscore", spoints);
+		}catch(Exception e){
+				Debug.LogWarning(e.Message);
 		}
 	}
 	private void addEntry(string name, int points) {
@@ -80,6 +126,6 @@ public class GameEndState : MonoBehaviour {
 		StartCoroutine(
 			HighscoreServer.AddEntry(name, points)
 		);
-		checkHighscore();
+		
 	}
 }
