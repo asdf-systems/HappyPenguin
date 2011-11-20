ce69c27c61cd3cb04c48b980d4b162cc4fb577b9
Friedrich Wessel @Blackdragon <wessel@sadf-systems.de>
2011-11-20 21:50:40 +0100

    working on Highscore

--- unity_project/Assets/Scripts/GUI/Elements/Highscore/Numbers.cs
+++ unity_project/Assets/Scripts/GUI/Elements/Highscore/Numbers.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using System.Collections;
+using Pux.Resources;
 
-public class Numbers : UIElementBehaviour<GUIStatics> {
+public class Numbers : Panel {
 
 	public string points{
 		get;
@@ -9,31 +10,21 @@ public class Numbers : UIElementBehaviour<GUIStatics> {
 	}
 
 	public int textureSize;
-	public GUIStyle zero;
-	public GUIStyle one;
-	public GUIStyle two;
-	public GUIStyle three;
-	public GUIStyle four;
-	public GUIStyle five;
-	public GUIStyle six;
-	public GUIStyle seven;
-	public GUIStyle eight;
-	public GUIStyle nine;
 
 	// Use this for initialization
-	void Start () {
-		points = "";
+	void Awake () {
+		points = string.Empty;
 	}
 
-	void OnGUI(){
-		GUIStyle[] numberStyles = {zero,one,two,three,four,five,six,seven,eight,nine};
-
+	public override void createGUIElement(){
+		
 		int number =0;
 		int xOff = 0;
 		for(int i=0; i < 7  && i < points.Length; i++){
+			Panel sign = ResourceManager.CreateInstance<GameObject>("Numbers/number"+number.ToString()).GetComponent<Panel>();
 			number = int.Parse(""+points[i]);
-			GeneralScreenGUI.Box(guiStatics, new Rect(positionX+xOff,positionY,textureSize,textureSize), "", numberStyles[number]);
-			xOff += textureSize - 15;
+			sign.VirtualRegionOnScreen.x  += xOff; 
+			xOff += (int)(sign.VirtualRegionOnScreen.width) - 15;
 		}
 
 	}
--- unity_project/Assets/Scripts/GUI/Elements/Highscore/TextFirst.cs
+++ unity_project/Assets/Scripts/GUI/Elements/Highscore/TextFirst.cs
@@ -1,33 +1,25 @@
 using UnityEngine;
 using System.Collections;
 
-public class TextFirst : UIElementBehaviour<GUIStatics> {
+public class TextFirst : TextPanel {
 
-	public int targetTextSize;
 	public int position;
 	private string username;
 	private int points;
+	
 
-	public void Start() {
+	void Start() {
 		username = "";
 		points = 0;
 		StartCoroutine(
 			HighscoreServer.GetHighscore(hs => {
 				username = hs[position-1].Name;
 				points = hs[position-1].Points;
+				this.GetComponent<Numbers>().points = points.ToString("D7");
+				Text = username;
+				
 			})
 		);
 	}
-
-	protected override void showElements(){
-		// Highscore name
-		inactiveStyle.fontSize = targetTextSize;
-		GeneralScreenGUI.Label(guiStatics, new Rect(positionX,positionY,128,128), username, inactiveStyle);
-		this.GetComponent<Numbers>().points = points.ToString("D7");
-	}
-
-
-
-	protected override void hit(){
-	}
+	
 }
