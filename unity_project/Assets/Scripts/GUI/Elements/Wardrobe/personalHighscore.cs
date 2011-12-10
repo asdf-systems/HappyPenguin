0b7094c4926dabbe5c179f8f8f6eebbfe3b8272b
Friedrich Wessel <asdf-systems@littlefoot.fritz.box>
2011-12-10 17:50:58 +0100

    fixed name Bug in Wardrobe, fixed Texts in Wardrobe and Credits for 4G

--- unity_project/Assets/Scripts/GUI/Elements/Wardrobe/personalHighscore.cs
+++ unity_project/Assets/Scripts/GUI/Elements/Wardrobe/personalHighscore.cs
@@ -7,6 +7,7 @@ public class personalHighscore : InteractionBehaviour {
 	
 	
 	private string username;
+	private TextPanel[]  textPanels;
 	
 	//protected override void showElements(){
 		/*textStyle.fontSize = targetTextSize;
@@ -25,28 +26,35 @@ public class personalHighscore : InteractionBehaviour {
 	//}
 	
 	void Start(){
+		textPanels = gameObject.GetComponents<TextPanel>() as TextPanel[];
 		username = GameStatics.Username;
-		TextPanel[] text = gameObject.GetComponents<TextPanel>() as TextPanel[];
-		if(text != null && text.Length >= 2){
-			text[0].Text = username; 
-			text[1].Text = "\nSCORE: " + GameStatics.FormatPoints(GameStatics.PersonalHighscore);
+		Debug.LogWarning("Start Username: " + username);
+		if(textPanels != null && textPanels.Length >= 2){
+			textPanels[0].Text = username; 
+			textPanels[1].Text = "SCORE: " + GameStatics.FormatPoints(GameStatics.PersonalHighscore);
 		} else{
 			EditorDebug.LogWarning("Personal Highscore need Two no TextPanels Attached, second is the name");
 		}
 	}
 	
 	public override void TextChanged(string text){
-		if(!text.StartsWith("         "))
-			GameStatics.Username = text;
+		if(textPanels != null && textPanels.Length > 1){
+			Debug.LogWarning("TextChanged: " + text);
+			string value = textPanels[0].Text;
+			if(value.Trim() != string.Empty)
+				GameStatics.Username = value;
+			Debug.LogWarning("Username: " + GameStatics.Username + "XX");	
+		}
+		
 	}
 	
-	private string extractUsername(string text){
+	/*private string extractUsername(string text){
 		string tmp = string.Empty;
 		for(int i=0; i < text.Length-5; i++){
 			tmp += text[i];
 		}
 		return tmp;
-	}
+	}*/
 	
 	
 	
